44dd2a22cfaf8bf95bc2d2410daf403cf8a5653c
Frand-Chen <Frand-Chen@163.com>
2019-02-12 19:53:55 +0800

    第五次

--- scripts/test_demo.py
+++ scripts/test_demo.py
@@ -12,7 +12,7 @@ class Test_allure:
     @allure.severity('blocker')
     # @allure.severity(allure.severity_level.CRITICAL)
     def test_fun1(self):
-        allure.attach('描述', '我是测试步骤00333555的描述～～～')
+        allure.attach('描述', '我是测试步骤00333的描述～～～')
         assert 1
 
     @allure.step(title="步骤二")
