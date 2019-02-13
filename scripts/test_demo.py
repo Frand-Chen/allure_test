d8a014fcedfe0ba07eeeb4a66bc20205ffffe951
Frand-Chen <Frand-Chen@163.com>
2019-02-13 10:20:00 +0800

    第六次变更

--- scripts/test_demo.py
+++ scripts/test_demo.py
@@ -18,7 +18,11 @@ class Test_allure:
     @allure.step(title="步骤二")
     @allure.severity('blocker')
     def test_fun2(self):
-        allure.attach('描述', '我是测试步骤00444的描述～～～')
+        # allure.attach('描述', '我是测试步骤00444的描述～～～')
+        imgfile = open(r'F:\allure_test1\picture1.png','rb').read()
+        print(type(imgfile))
+        # print(imgfile)
+        allure.attach('成功截图', str(imgfile),allure.attachment_type.PNG)
         print(4)
         assert 1
 
